5c81a0da019f882e267ee7f081f613dad0792616
Musab Al Mahi <musabalmahi53@gmail.com>
2023-12-10 22:25:15 +0600

    sending payment email via mailgun and accept email in my gmail account

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -88,7 +88,7 @@ const Login = () => {
                         </div>
                         <div className="form-control mt-6">
                             {/* TODO: apply disabled for re captcha */}
-                            <input disabled={false} className="btn btn-primary" type="submit" value="Login" />
+                            <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
                         </div>
                     </form>
                     <p className='px-6'><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
